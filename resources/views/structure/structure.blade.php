01489f5a4118af2d1501ee34b846eb8a62de957f
lina-22 <haquefahinur@gmail.com>
2022-04-10 17:39:27 +0200

    structe under construction

--- resources/views/structure/structure.blade.php
+++ resources/views/structure/structure.blade.php
@@ -24,6 +24,23 @@ Structure
     à la vente de feuilles Letraset contenant des
     passages du Lorem Ipsum, et, plus récemment,
     par son</h4>
+
+    <form action="">
+        helloooo
+    </form>
+    <form action="{{ route('structure.store') }}" method="post">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" name="name">
+        </div>
+        <div class="mb-3">
+            <label for="type" class="form-label">Type</label>
+            <textarea name="type" id="type" class="form-control" rows="5"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
 {{-- <table class="table">
     @csrf
     <thead>
