4e9f1557a7c4cba9f51816f1b1b3a60b82987041
unknown <haquefahinur@gmail.com>
2022-04-12 16:28:41 +0200

    page welcomeEmmaus start to work

---
+++ resources/views/welcomeEmmaus.blade.php
@@ -0,0 +1,29 @@
+@extends("template")
+@section('titre')
+    Page Create oneStructure
+@endsection
+@section('contenu')
+
+<div>
+    <section class="first_landing">
+        <div id="left_side">
+        <img src="./main_img/abbe.jpg" alt="">
+        </div>
+        <div id="right_side">
+            <h1>EMMAÜS SYNERGIE</h1>
+            <p>à la vente de feuilles Letraset contenant des 
+                passages du Lorem Ipsum, et, plus récemment, 
+                par son
+                
+                 inclusion dans des applications de mise en 
+                page de texte, comme Aldus PageMaker
+                
+                à la vente de feuilles Letraset contenant des 
+                passages du Lorem Ipsum, et, plus récemment, 
+                par son</p>
+        </div>
+
+    </section>
+</div>
+
+@endsection
